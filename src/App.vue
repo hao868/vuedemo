a385709e47b9ed8fc99960ca703c3e30a91c26af
zgh868 <fangqiai_hao083@qq.com>
2019-08-31 14:53:15 +0800

    2019.8.31

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <router-view />
+    <el-container>
+      <!-- header -->
+      <el-header>
+        <router-link to="/home">Home</router-link>
+        <router-link to="/news">News</router-link>
+        <router-link to="/somes">Somes</router-link>
+        <span class="headers">{{headersTop}}</span>
+      </el-header>
+
+      <transition name="fade">
+        <router-view></router-view>
+      </transition>
+      
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      headersTop: "这是一个没有感情的Header!"
+    };
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  /* color: #2c3e50; */
-  /* margin-top: 60px; */
 }
 
-ul,li,ol{
+ul,
+li,
+ol {
   list-style: none;
   margin: 0;
   padding: 0;
+}
+
+a{
+  text-decoration: none;
+  color: #333;
+}
+
+.headers {
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  letter-spacing: 1px;
+}
+
+.el-header {
+  background-color: #1abc9c;
+  color: #fff;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+  text-align: center;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home/Home'
+import News from '../components/News/News'
+import Somes from '../components/Somes/Somes'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/Home',
-      name:'Home',
-      component:Home
+      path: '/home',component:Home,
     },
     {
-      path:'*',redirect:'/Home'
+      path: '/news',component:News,
+    },
+    {
+      path: '/somes',component:Somes,
+    },
+    {
+      path: '*',redirect:'/home',
     }
   ]
 })
